39af06186f1229a0d80d273123a22f75f7b2e948
gschorer <gschorer@vistaprint.com>
2014-04-25 11:15:57 -0400

    Adding an EmbeddedResourceVirtualPathProvider which can resolve embedded resources to semi-reasonable paths (A.B.C.txt -> A/B/C.txt)

--- src/NServiceKit.Interfaces/IO/IVirtualFile.cs
+++ src/NServiceKit.Interfaces/IO/IVirtualFile.cs
@@ -2,16 +2,49 @@
 
 namespace NServiceKit.IO
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IVirtualFile : IVirtualNode
     {
+        /// <summary>
+        /// Gets the virtual path provider.
+        /// </summary>
+        /// <value>
+        /// The virtual path provider.
+        /// </value>
         IVirtualPathProvider VirtualPathProvider { get; }
 
+        /// <summary>
+        /// Gets the extension.
+        /// </summary>
+        /// <value>
+        /// The extension.
+        /// </value>
         string Extension { get; }
 
+        /// <summary>
+        /// Gets the file hash.
+        /// </summary>
+        /// <returns></returns>
         string GetFileHash();
 
+        /// <summary>
+        /// Opens the file for reading.
+        /// </summary>
+        /// <returns></returns>
         Stream OpenRead();
+
+        /// <summary>
+        /// Opens a StreamReader.
+        /// </summary>
+        /// <returns></returns>
         StreamReader OpenText();
+
+        /// <summary>
+        /// Reads all text.
+        /// </summary>
+        /// <returns></returns>
         string ReadAllText();
     }
 }
--- src/NServiceKit.Interfaces/IO/IVirtualPathProvider.cs
+++ src/NServiceKit.Interfaces/IO/IVirtualPathProvider.cs
@@ -9,89 +9,98 @@ namespace NServiceKit.IO
     public interface IVirtualPathProvider
     {
         /// <summary>
-        /// 
+        /// Gets the root directory.
         /// </summary>
+        /// <value>
+        /// The root directory.
+        /// </value>
 		IVirtualDirectory RootDirectory { get; }
 
         /// <summary>
-        /// 
+        /// Gets the virtual path separator.
         /// </summary>
+        /// <value>
+        /// The virtual path separator.
+        /// </value>
         string VirtualPathSeparator { get; }
 
         /// <summary>
-        /// 
+        /// Gets the real path separator.
         /// </summary>
+        /// <value>
+        /// The real path separator.
+        /// </value>
         string RealPathSeparator { get; }
 
         /// <summary>
-        /// 
+        /// Combines the virtual path.
         /// </summary>
-        /// <param name="basePath"></param>
-        /// <param name="relativePath"></param>
+        /// <param name="basePath">The base path.</param>
+        /// <param name="relativePath">The relative path.</param>
         /// <returns></returns>
         string CombineVirtualPath(string basePath, string relativePath);
 
         /// <summary>
-        /// 
+        /// Files the exists.
         /// </summary>
-        /// <param name="virtualPath"></param>
+        /// <param name="virtualPath">The virtual path.</param>
         /// <returns></returns>
         bool FileExists(string virtualPath);
 
         /// <summary>
-        /// 
+        /// Directories the exists.
         /// </summary>
-        /// <param name="virtualPath"></param>
+        /// <param name="virtualPath">The virtual path.</param>
         /// <returns></returns>
         bool DirectoryExists(string virtualPath);
 
         /// <summary>
-        /// 
+        /// Gets the file.
         /// </summary>
-        /// <param name="virtualPath"></param>
+        /// <param name="virtualPath">The virtual path.</param>
         /// <returns></returns>
         IVirtualFile GetFile(string virtualPath);
 
         /// <summary>
-        /// 
+        /// Gets the file hash.
         /// </summary>
-        /// <param name="virtualPath"></param>
+        /// <param name="virtualPath">The virtual path.</param>
         /// <returns></returns>
         string GetFileHash(string virtualPath);
 
         /// <summary>
-        /// 
+        /// Gets the file hash.
         /// </summary>
-        /// <param name="virtualFile"></param>
+        /// <param name="virtualFile">The virtual file.</param>
         /// <returns></returns>
         string GetFileHash(IVirtualFile virtualFile);
 
         /// <summary>
-        /// 
+        /// Gets the directory.
         /// </summary>
-        /// <param name="virtualPath"></param>
+        /// <param name="virtualPath">The virtual path.</param>
         /// <returns></returns>
         IVirtualDirectory GetDirectory(string virtualPath);
 
         /// <summary>
-        /// 
+        /// Gets all matching files.
         /// </summary>
-        /// <param name="globPattern"></param>
-        /// <param name="maxDepth"></param>
+        /// <param name="globPattern">The glob pattern.</param>
+        /// <param name="maxDepth">The maximum depth.</param>
         /// <returns></returns>
         IEnumerable<IVirtualFile> GetAllMatchingFiles(string globPattern, int maxDepth = Int32.MaxValue);
 
         /// <summary>
-        /// 
+        /// Determines whether [is shared file] [the specified virtual file].
         /// </summary>
-        /// <param name="virtualFile"></param>
+        /// <param name="virtualFile">The virtual file.</param>
         /// <returns></returns>
         bool IsSharedFile(IVirtualFile virtualFile);
 
         /// <summary>
-        /// 
+        /// Determines whether [is view file] [the specified virtual file].
         /// </summary>
-        /// <param name="virtualFile"></param>
+        /// <param name="virtualFile">The virtual file.</param>
         /// <returns></returns>
         bool IsViewFile(IVirtualFile virtualFile);
     }

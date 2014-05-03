6d7919bb21c2c10f3b8bdcc4ae68a6eaad29e5d4
Jeff Kwan <mirthy@gmail.com>
2014-05-03 01:26:03 -0400

    Upgrade travis build to Mono 3.4.0

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 export EnableNuGetPackageRestore=true
 mono ./src/.nuget/NuGet.exe install NServiceKit.Text
-xbuild ./src/NServiceKit.sln
+xbuild ./src/NServiceKit.sln /p:TargetFramework=net40 /p:Configuration=Release

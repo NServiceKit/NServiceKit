aaf114537d750ba19a22364cf796924bf17a0442
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2015-10-13 21:23:55 +0300

    Added null validation for parameter 'format' of method HttpResponseFilter.GetFormatContentType; Implementation restored so that you can get media type if you registered format in non-lower case; Added a couple of unit tests demonstrating the bug and validating behaviour.

--- tests/NServiceKit.Common.Tests/Web/HttpResponseFilterTests.cs
+++ tests/NServiceKit.Common.Tests/Web/HttpResponseFilterTests.cs
@@ -1,4 +1,6 @@
-﻿using NServiceKit.Common.Web;
+﻿using System;
+
+using NServiceKit.Common.Web;
 
 using NUnit.Framework;
 
@@ -20,21 +22,21 @@ namespace NServiceKit.Common.Tests.Web
         }
 
         [Test]
-        public void GetFormatContentType_WhenFormatIsUppercaseJSON_ShouldReturnJsonMediaType()
-        {
-            // Arrange
-            // Act
-            // Assert
-            AssertThatGetFormatContentTypeReturnsMediaTypeForFormat("JSON", "application/json");
-        }
-
-        [Test]
         public void GetFormatContentType_WhenFormatIsMixedcaseJSON_ShouldReturnJsonMediaType()
         {
             // Arrange
             // Act
             // Assert
             AssertThatGetFormatContentTypeReturnsMediaTypeForFormat("JsOn", "application/json");
+        }
+
+        [Test]
+        public void GetFormatContentType_WhenFormatIsUppercaseJSON_ShouldReturnJsonMediaType()
+        {
+            // Arrange
+            // Act
+            // Assert
+            AssertThatGetFormatContentTypeReturnsMediaTypeForFormat("JSON", "application/json");
         }
 
         [Test]
@@ -91,6 +93,69 @@ namespace NServiceKit.Common.Tests.Web
             AssertThatGetFormatContentTypeReturnsMediaTypeForFormat("JSV", "application/jsv");
         }
 
+        [Test]
+        public void GetFormatContentType_WhenFormatIsInLowercaseAndRegistered_ShouldReturnRegisteredMediaType()
+        {
+            // Arrange
+            HttpResponseFilter responseFilter = new HttpResponseFilter();
+            responseFilter.ContentTypeFormats.Add("registered_format", "registeredMediaType");
+
+            // Act
+            string contentType = responseFilter.GetFormatContentType("registered_format");
+
+            // Assert
+            Assert.That(contentType, Is.EqualTo("registeredMediaType"));
+        }
+
+        [Test]
+        public void GetFormatContentType_WhenFormatIsInUppercaseAndRegistered_ShouldReturnRegisteredMediaType()
+        {
+            // Arrange
+            HttpResponseFilter responseFilter = new HttpResponseFilter();
+            responseFilter.ContentTypeFormats.Add("REGISTERED_FORMAT", "registeredMediaType");
+
+            // Act
+            string contentType = responseFilter.GetFormatContentType("REGISTERED_FORMAT");
+
+            // Assert
+            Assert.That(contentType, Is.EqualTo("registeredMediaType"));
+        }
+
+        [Test]
+        public void GetFormatContentType_WhenFormatIsUnregistered_ShouldReturnNull()
+        {
+            // Arrange
+            // Act
+            // Assert
+            AssertThatGetFormatContentTypeReturnsNullForFormat("unregisteredFormat");
+        }
+
+        [Test]
+        public void GetFormatContentType_WhenFormatIsEmpty_ShouldReturnNull()
+        {
+            // Arrange
+            string emptyFormat = string.Empty;
+
+            // Act
+            // Assert
+            AssertThatGetFormatContentTypeReturnsNullForFormat(emptyFormat);
+        }
+
+        [Test]
+        public void GetFormatContentType_WhenFormatIsNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            HttpResponseFilter responseFilter = new HttpResponseFilter();
+
+            string nullFormat = null;
+
+            // Act
+            TestDelegate getFormatContentTypeAction = () => responseFilter.GetFormatContentType(nullFormat);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(getFormatContentTypeAction);
+        }
+
         private void AssertThatGetFormatContentTypeReturnsMediaTypeForFormat(string format, string expectedMediaType)
         {
             // Arrange
@@ -101,6 +166,18 @@ namespace NServiceKit.Common.Tests.Web
 
             // Assert
             Assert.That(contentType, Is.EqualTo(expectedMediaType));
+        }
+
+        private void AssertThatGetFormatContentTypeReturnsNullForFormat(string format)
+        {
+            // Arrange
+            HttpResponseFilter responseFilter = new HttpResponseFilter();
+
+            // Act
+            string contentType = responseFilter.GetFormatContentType(format);
+
+            // Assert
+            Assert.That(contentType, Is.Null);
         }
     }
 }

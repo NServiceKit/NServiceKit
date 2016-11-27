0f75aa5fb55fa2ea1ed44918761867e0b0d8f26f
Peter Gill <peter@majorsilence.com>
2016-11-27 10:36:46 -0330

    Migrate to automatic nuget restore
    See http://docs.nuget.org/ndocs/consume-packages/package-restore#migrating-to-automatic-restore
    
    Used scripts from https://github.com/owen2/AutomaticPackageRestoreMigrationScript

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-export EnableNuGetPackageRestore=true
-mono ./src/.nuget/NuGet.exe install NServiceKit.Text
+set -e # exit on first error
+set -u # exit on using unset variable
+
+nuget restore ./src/NServiceKit.sln -configfile nuget.config
+nuget install NServiceKit.Text
 xbuild ./src/NServiceKit.sln /p:TargetFramework=net40 /p:Configuration=Release
